56b3396e9b051ad51d30d7ddea21b73b3d813de2
nitintf <nitinpanwar0802@gmail.com>
2023-09-26 11:28:04 +0530

    feat: allocate books

---
+++ algorithms/searching/binary/allocate-books/index.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int countStudents(vector<int>& arr, int pages) {
+  int n = arr.size();
+  int students = 1;
+  long long pagesCount = 0;
+
+  for (int i = 0; i < n; i++) {
+    if (pagesCount + arr[i] <= pages) {
+      pagesCount += arr[i];
+    }
+    else {
+      students++;
+      pagesCount = arr[i];
+    }
+  }
+
+  return students;
+}
+
+int findPages(vector<int>& arr, int n, int m) {
+  // book allocation impossible:
+  if (m > n) {
+    return -1;
+  }
+
+  int low = *max_element(arr.begin(), arr.end());
+  int high = accumulate(arr.begin(), arr.end(), 0);
+
+  while (low <= high) {
+    int mid = (low + high) / 2;
+
+    if (countStudents(arr, mid) <= m) {
+      high = mid - 1;
+    }
+    else {
+      low = mid + 1;
+    }
+  }
+
+  return low;
+}
