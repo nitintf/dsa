3d2eac04ac538f8f8c5283e99cff7a405b9b3fdf
Nitin <55453926+nitintf@users.noreply.github.com>
2024-01-02 18:26:45 +0530

    Time: 345 ms (5.08%), Space: 71.8 MB (28.77%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        
+        // Edge case
+        if (n == 1) {
+            return 1;
+        }
+        
+        int right = 0, left =0;
+
+        int max_count = 0;
+
+        unordered_map<int, int> fruitsMap;
+        
+        while (right < n) {
+            int f = fruits[right];
+            fruitsMap[f]++;
+            
+            if(fruitsMap.size() < 2) {
+                right++;
+            } else if (fruitsMap.size() == 2) {
+                max_count = max(max_count, right - left + 1);
+                right++;
+            } else if (fruitsMap.size() > 2) {
+                while(fruitsMap.size() > 2) {
+                    fruitsMap[fruits[left]]--;
+                    if (fruitsMap[fruits[left]] == 0) {
+                        fruitsMap.erase(fruits[left]);
+                    }
+                    left++;
+                }
+                right++;
+            }
+        }
+        
+        if (fruitsMap.size() == 1) {
+            auto fr = fruitsMap.begin();
+            return fr->second;
+        }
+
+        return max_count;
+    }
+};
