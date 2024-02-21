f4eb890705120ed719e8610e5c7c200ad00903dd
nitintf <nitinpanwar0802@gmail.com>
2024-02-21 11:26:34 +0530

    dp: best time to buy and sell stock II with tabulation and space optimized [122]

--- problems/dynamic-programming/best-time-to-buy-and-sell-stock-II-122/solution.cpp
+++ problems/dynamic-programming/best-time-to-buy-and-sell-stock-II-122/solution.cpp
@@ -50,6 +50,79 @@ public:
     vector<vector<int>> dp(n, vector<int>(2, -1));
     return findMaxProfit(prices, 0, 1, dp);
   }
+
+  /**
+   [ Tabulation ]
+    - base case
+    - start the loops
+    - copy recurrence
+  */
+  int maxProfitTabulation(vector<int>& prices) {
+    int n = prices.size();
+    vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+    for (int idx = n - 1; idx >= 0; idx--) {
+      for (int canBuy = 0; canBuy < 2; canBuy++) {
+        int profit = 0;
+
+        if (canBuy) {
+          // Buying
+          int take = -prices[idx] + dp[idx + 1][0];
+          int notTake = dp[idx + 1][1];
+
+          profit = max(take, notTake);
+        }
+        else {
+          // Selling
+          int take = prices[idx] + dp[idx + 1][1];
+          int notTake = dp[idx + 1][0];
+
+          profit = max(take, notTake);
+        }
+
+        dp[idx][canBuy] = profit;
+      }
+    }
+
+    return dp[0][1];
+  }
+
+
+  /*
+  [ Space optimized ]
+    -
+  */
+  int maxProfitSpaceOptimized(vector<int>& prices) {
+    int n = prices.size();
+    vector<int> next(2, 0), curr(2, 0);
+
+    for (int idx = n - 1; idx >= 0; idx--) {
+      for (int canBuy = 0; canBuy < 2; canBuy++) {
+        int profit = 0;
+
+        if (canBuy) {
+          // Buying
+          int take = -prices[idx] + next[0];
+          int notTake = next[1];
+
+          profit = max(take, notTake);
+        }
+        else {
+          // Selling
+          int take = prices[idx] + next[1];
+          int notTake = next[0];
+
+          profit = max(take, notTake);
+        }
+
+        curr[canBuy] = profit;
+      }
+
+      next = curr;
+    }
+
+    return next[1];
+  }
 };
 
 int main() {}
