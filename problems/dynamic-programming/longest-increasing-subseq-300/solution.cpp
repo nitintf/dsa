5376ff8d4d7ba320595c8124822d9980d8336a9c
nitintf <nitinpanwar0802@gmail.com>
2024-02-24 07:57:00 +0530

    dp: longest increasign subseq with tabulation, space optimized and backtracking for printing LIS [300]

--- problems/dynamic-programming/longest-increasing-subseq-300/solution.cpp
+++ problems/dynamic-programming/longest-increasing-subseq-300/solution.cpp
@@ -34,16 +34,86 @@ public:
     return findLIS(nums, 0, -1, dp);
   }
 
-  // @TODO: undestand tabulation
   int lengthOfLISTabulation(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+    for (int idx = n - 1; idx >= 0; idx--) {
+      for (int prev_idx = idx - 1; prev_idx >= -1; prev_idx--) {
+        int len = dp[idx + 1][prev_idx + 1];
+        if (prev_idx == -1 || nums[prev_idx] < nums[idx]) {
+          len = max(len, 1 + dp[idx + 1][idx + 11]);
+        }
+
+        dp[idx][prev_idx + 1] = len;
+      }
+    }
+
+    return dp[0][0];
+  }
+
+  int lengthOfLISSpaceOptimized(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> next(n + 1, 0), curr(n + 1, 0);
+
+    for (int idx = n - 1; idx >= 0; idx--) {
+      for (int prev_idx = idx - 1; prev_idx >= -1; prev_idx--) {
+        int len = next[prev_idx + 1];
+        if (prev_idx == -1 || nums[prev_idx] < nums[idx]) {
+          len = max(len, 1 + next[idx + 11]);
+        }
+
+        curr[prev_idx + 1] = len;
+      }
+
+      next = curr;
+    }
+
+    return curr[0];
+  }
+
+  int lengthOfLISSpaceOpt(vector<int>& nums) {
     int n = nums.size();
     vector<int> dp(n, 1);
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < i; j++) {
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < i; ++j)
+        if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+          dp[i] = dp[j] + 1;
+    return *max_element(dp.begin(), dp.end());
+  }
 
+  vector<int> printLIS(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> dp(n, 1), hash(n);
+
+    int maxi = 1, lastIndex = 0;
+
+    for (int i = 0; i < n; ++i) {
+      hash[i] = i;
+      for (int j = 0; j < i; ++j) {
+        if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+          dp[i] = dp[j] + 1;
+          hash[i] = j;
+        }
+      }
+
+      if (dp[i] > maxi) {
+        maxi = dp[i];
+        lastIndex = i;
       }
     }
+
+    vector<int> temp;
+    temp.push_back(nums[lastIndex]);
+    while (hash[lastIndex] != lastIndex) {
+      lastIndex = hash[lastIndex];
+      temp.push_back(nums[lastIndex]);
+    }
+
+    reverse(temp.begin(), temp.end());
+
+    return temp;
   }
 
   int lengthOfLISBinarySearch(vector<int>& nums) {}
