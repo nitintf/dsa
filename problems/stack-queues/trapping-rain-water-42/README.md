d1a64b6010a7b8060b44bd1de9efccaa1c0687e6
nitintf <nitinpanwar0802@gmail.com>
2024-06-03 06:23:03 +0530

    stack: trapping rain watter brute force approach [42]

---
+++ problems/stack-queues/trapping-rain-water-42/README.md
@@ -0,0 +1,1 @@
+problems/stack-queues/trapping-rain-water-42
---
+++ problems/stack-queues/trapping-rain-water-42/solution.cpp
@@ -0,0 +1,55 @@
+#include <climits>
+#include<vector>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+/**
+Notes
+*/
+
+class Solution {
+    private:
+        int right(int idx, int n, vector<int> &height) {
+            int maxx = height[idx];
+
+            for (int i = idx; i < n; i++) {
+                maxx = max(maxx, height[i]);
+            }
+
+            return maxx;
+        }
+
+        int left(int idx, vector<int> &height) {
+            int maxx = height[idx];
+
+            for (int i = idx; i >= 0; i--) {
+                maxx = max(maxx, height[i]);
+            }
+
+            return maxx;
+        }
+
+    public:
+
+        // brute force approach, we find the max height on left and right find the min of that and subtract the current height thats the unit of water we can store at that index,
+        // TC: O(n*n), SC: O(1)
+        int trap(vector<int> &height) {
+            int totalWater= 0;
+            int n = height.size();
+
+            for (int i = 0; i < n; i++) {
+                totalWater += min(left(i, height), right(i, n, height)) - height[i];
+            }
+
+            return totalWater;
+        }
+
+
+        int trapOpt(vector<int> &height) {
+
+        }
+};
+
+int main() {}
